2cf2f6a03757cbd3e08a14d2a1bfb9d59f2ef0da
VascoO21 <VascoKing21@mhs.mdp.ac.id>
2022-10-03 11:15:57 +0700

    PindahLayout

--- app/src/main/java/com/if3b/pindahlayout/MainActivity.java
+++ app/src/main/java/com/if3b/pindahlayout/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Layout;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btnPindah = findViewById(R.id.btn_pindah);
+        getActionBar().setTitle("Raijin");
         btnPindah.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
